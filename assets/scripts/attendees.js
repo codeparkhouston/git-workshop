4a2ac82f9b79769c1cbc07b7baebfeb79455a92d
pandafulmanda <pandafulmanda@gmail.com>
2016-03-03 23:16:08 -0600

    update rsvps and update photos to be https

--- assets/scripts/attendees.js
+++ assets/scripts/attendees.js
@@ -1,91 +1,128 @@
 var attendees = [{
     name: 'Karl',
-    photo: '//avatars0.githubusercontent.com/u/5505099?v=3&s=400'
+    photo: 'https://avatars0.githubusercontent.com/u/5505099?v=3&s=400'
   },{
     name: 'Tim',
-    photo: 'http://photos3.meetupstatic.com/photos/member/1/5/f/d/thumb_243245629.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/1/5/f/d/thumb_243245629.jpeg'
   },{
     name: 'Stephen'
   },{
     name: 'Joe',
-    photo: 'http://photos3.meetupstatic.com/photos/member/a/0/1/1/thumb_251920977.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/a/0/1/1/thumb_251920977.jpeg'
   },{
     name: 'Sahil',
-    photo: '//avatars1.githubusercontent.com/u/684975?v=3&s=460'
+    photo: 'https://avatars1.githubusercontent.com/u/684975?v=3&s=460'
   },{
     name: 'Gboyega',
-    photo: 'http://photos4.meetupstatic.com/photos/member/7/8/a/9/member_251550889.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/7/8/a/9/member_251550889.jpeg'
   },{
     name: 'Jenny',
-    photo: 'http://photos1.meetupstatic.com/photos/member/5/a/e/7/thumb_253463271.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/5/a/e/7/thumb_253463271.jpeg'
   },{
     name: 'David'
   },{
     name: 'Chris',
-    photo: 'http://photos2.meetupstatic.com/photos/member/6/1/b/e/member_80965022.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/6/1/b/e/member_80965022.jpeg'
   },{
     name: 'Nitin',
-    photo: 'http://photos1.meetupstatic.com/photos/member/8/9/7/8/member_143555192.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/8/9/7/8/member_143555192.jpeg'
   },{
     name: 'Eloy',
-    photo: 'http://photos4.meetupstatic.com/photos/member/3/e/7/member_12480999.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/3/e/7/member_12480999.jpeg'
   },{
     name: 'Jin',
-    photo: '//avatars2.githubusercontent.com/u/6503836?v=3&s=460'
+    photo: 'https://avatars2.githubusercontent.com/u/6503836?v=3&s=460'
   },{
     name: 'Michelle',
-    photo: '//avatars3.githubusercontent.com/u/5847076?v=3&s=400'
+    photo: 'https://avatars3.githubusercontent.com/u/5847076?v=3&s=400'
   },{
     name: 'Karl',
-    photo: 'http://photos2.meetupstatic.com/photos/member/5/e/3/9/member_19404121.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/5/e/3/9/member_19404121.jpeg'
   },{
     name: 'Andrea',
-    photo: 'http://photos1.meetupstatic.com/photos/member/c/5/4/6/member_227810502.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/c/5/4/6/member_227810502.jpeg'
   },{
     name: 'Alvaro',
-    photo: '//avatars0.githubusercontent.com/u/12219986?v=3&s=400'
+    photo: 'https://avatars0.githubusercontent.com/u/12219986?v=3&s=400'
   },{
     name: 'Susann',
-    photo: 'http://photos1.meetupstatic.com/photos/member/6/a/b/b/member_252807323.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/6/a/b/b/member_252807323.jpeg'
   },{
     name: 'Ganesh',
-    photo: 'http://photos2.meetupstatic.com/photos/member/b/3/a/2/member_250185986.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/b/3/a/2/member_250185986.jpeg'
   },{
     name: 'Heather'
   },{
     name: 'Michele'
   },{
     name: 'Rory',
-    photo: 'http://photos3.meetupstatic.com/photos/member/e/1/7/a/member_10377722.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/e/1/7/a/member_10377722.jpeg'
   },{
     name: 'David',
-    photo: 'http://photos1.meetupstatic.com/photos/member/3/3/c/4/member_155893252.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/3/3/c/4/member_155893252.jpeg'
   },{
     name: 'Pavan'
   },{
     name: 'Jennifer',
-    photo: 'http://photos2.meetupstatic.com/photos/member/5/b/c/e/thumb_213503502.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/5/b/c/e/thumb_213503502.jpeg'
   },{
     name: 'Wanjun',
-    photo: 'http://photos4.meetupstatic.com/photos/member/1/5/e/2/member_138005602.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/1/5/e/2/member_138005602.jpeg'
   },{
     name: 'Nha',
-    photo: '//avatars3.githubusercontent.com/u/14111728?v=3&s=400'
+    photo: 'https://avatars3.githubusercontent.com/u/14111728?v=3&s=400'
   },{
     name: 'Charles',
-    photo: 'http://photos2.meetupstatic.com/photos/member/6/4/4/6/member_244285670.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/6/4/4/6/member_244285670.jpeg'
   },{
     name: 'Matthew'
   },{
     name: 'Amanda',
-    photo: '//avatars2.githubusercontent.com/u/2483873?v=3&s=460'
+    photo: 'https://avatars2.githubusercontent.com/u/2483873?v=3&s=460'
   },{
     name: 'Neeraj',
-    photo: '//avatars2.githubusercontent.com/u/1100564?v=3&s=460'
+    photo: 'https://avatars2.githubusercontent.com/u/1100564?v=3&s=460'
   },{
     name: 'Ju',
-    photo: 'http://photos3.meetupstatic.com/photos/member/2/c/7/6/thumb_140591382.jpeg'
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/2/c/7/6/thumb_140591382.jpeg'
   },{
     name: 'John',
-    photo: 'http://photos2.meetupstatic.com/photos/member/6/8/8/0/thumb_36746752.jpeg'    
-}];
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/6/8/8/0/thumb_36746752.jpeg'    
+  },{
+    name: 'Cheri',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/5/d/2/1/thumb_249443841.jpeg'
+  },{
+    name: 'Victor'
+  },{
+    name: 'Victoria'
+  },{
+    name: 'Seth',
+    photo: 'https://avatars3.githubusercontent.com/u/12239654?v=3&s=400'
+  },{
+    name: 'Kimal',
+    photo: 'https://avatars1.githubusercontent.com/u/8397660?v=3&s=400'
+  },{
+    name: 'Vanessa',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/c/a/8/2/thumb_249771842.jpeg'
+  },{
+    name: 'Jeffrey'
+  },{
+    name: 'Ben',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/a/1/0/6/thumb_85721222.jpeg'
+  },{
+    name:'Yasin'
+  },{
+    name: 'Ahmed',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/7/e/8/8/thumb_72152392.jpeg'
+  },{
+    name: 'Raymond'
+  },{
+    name: 'Tram',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/7/f/f/e/thumb_254012766.jpeg'
+  },{
+    name: 'Raymond',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/5/d/a/3/thumb_245543971.jpeg'
+  },{
+    name: 'Toan',
+    photo: 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/9/3/f/d/thumb_254437885.jpeg'
+  }];
